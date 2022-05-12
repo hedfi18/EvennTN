77161a4eb85adce46290d48f010779e2b56449e8
hedfi18 <92983584+hedfi18@users.noreply.github.com>
2022-05-12 09:19:25 +0100

    Add files via upload

---
+++ Forum.php
@@ -0,0 +1,192 @@
+
+<?php
+                            require 'D:/xampp/htdocs/web1/test/Controller/topicC.php';
+
+                        
+                            $TopicC = new topicC();
+                            $Topics = $TopicC->affichertopic();
+                        ?>
+<?php require 'Header.php'; ?>
+    <html lang="en">
+    
+    
+    <head>
+    
+    
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css" >
+    <!-- Icon -->
+    <link rel="stylesheet" type="text/css" href="assets/fonts/line-icons.css">
+    <!-- Slicknav -->
+    <link rel="stylesheet" type="text/css" href="assets/css/slicknav.css">
+    <!-- Nivo Lightbox -->
+    <link rel="stylesheet" type="text/css" href="assets/css/nivo-lightbox.css" >
+    <!-- Animate -->
+    <link rel="stylesheet" type="text/css" href="assets/css/animate.css">
+    <!-- Main Style -->
+    <link rel="stylesheet" type="text/css" href="assets/css/main.css">
+    <!-- Responsive Style -->
+    <link rel="stylesheet" type="text/css" href="assets/css/responsive.css">
+    <!--<link rel="stylesheet"  href="event.css">-->
+    <!-- CSS here -->
+        <link rel="stylesheet" href="assets1/css/style.css">
+</head>
+
+    
+
+
+
+
+
+  
+
+
+</head>
+
+
+
+    <body>
+    
+      <!--? Hero Area Start-->
+      <div class="slider-area ">
+            <div class="single-slider slider-height2 d-flex align-items-center">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-xl-12">
+                            <div class="hero-cap text-center">
+                                <h2>Forum</h2>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+       
+<?php
+    require_once 'D:/xampp/htdocs/web1/test/View/connexiondb.php';
+$connexiondb=connexiondb::getConnexion();
+
+if (isset($_POST["submit"])) {
+	$str = $_POST["search"];
+	$sth = $connexiondb->prepare("SELECT * FROM `topic` WHERE titre = '$str'");
+
+	$sth->setFetchMode(PDO:: FETCH_OBJ);
+	$sth -> execute();
+
+	if($Topic = $sth->fetch())
+	{
+		?>
+		
+        <article class="blog_item">
+                                <div class="blog_item_img">
+                                    <img class="card-img rounded-0" src="assets1/img/blog/single_blog_2.png" alt="">
+                                    
+                                    <a href="#" class="blog_item_date">
+                                        <h3>15</h3>
+                                        <p>Jan</p>
+                                    </a>
+                                </div>
+
+                                <div class="blog_details">
+                                    <a class="d-inline-block" href="regle.php">
+                                        <h2><?php echo $Topic->titre; ?></h2>
+                                        <h4><?php echo $Topic->descrip; ?></h4>
+                                    </a>
+                                    <p><?php echo $Topic->contenu; ?></p>
+                                    <ul class="blog-info-link">
+                                        <li><a href="#"><i class="fa fa-user"></i> Travel, Lifestyle</a></li>
+                                        <li><a href="comments.php?id=<?= $Topic['idtopic']; ?>"><i class="fa fa-comments"></i>  Comments</a></li>
+                                    </ul>
+                                </div>
+                            </article>
+        
+       
+<?php 
+	}
+		
+		
+		else{
+			echo "Name Does not exist";
+        }
+    }
+    ?>
+    
+   
+    <article class="blog_item">
+                                <div class="blog_item_img">
+                                    <img class="card-img rounded-0" src="assets1/img/blog/single_blog_2.png" alt="">
+                                    <a href="#" class="blog_item_date">
+                                        <h3>15</h3>
+                                        <p>Jan</p>
+                                    </a>
+                                </div>
+
+                                <div class="blog_details">
+                                    <a class="d-inline-block" href="regle.php">
+                                        <h2>Forum Règles:</h2>
+                                    </a>
+                                    <p>CI, vous trouvez les Règles du forum que vous devez lire avant de mettre un commentaire.</p>
+                                    <ul class="blog-info-link">
+                                        <li><a href="#"><i class="fa fa-user"></i> Travel, Lifestyle</a></li>
+                                        <li><a href="#"><i class="fa fa-comments"></i> Comments</a></li>
+                                    </ul>
+                                </div>
+                            </article>
+    
+          
+            
+ <?php                 
+                
+                    $i=0;
+                        foreach ($Topics as $Topic) {
+                         $i++;  
+            
+                ?>
+              
+              <article class="blog_item">
+                                <div class="blog_item_img">
+                                    <img class="card-img rounded-0" src="assets1/img/blog/single_blog_2.png" alt="">
+                                    <a href="#" class="blog_item_date">
+                                        <h3>15</h3>
+                                        <p>Jan</p>
+                                </div>
+
+                                <div class="blog_details">
+                                    <a class="d-inline-block" href="regle.php">
+                                        <h2><a <?= $Topic['idtopic']; ?>>Topic <?php echo $i; ?>: <?= $Topic['titre']; ?></a></h2>
+                                        <h4><?= $Topic['descrip']; ?></h4>
+                                    </a>
+                                    <p><?= $Topic['contenu']; ?></p>
+                                    <ul class="blog-info-link">
+                                        <li><a href="#"><i class="fa fa-user"></i> Travel, Lifestyle</a></li>
+                                        <li><a href="comments.php?id=<?= $Topic['idtopic']; ?>"><i class="fa fa-comments"></i>  Comments</a></li>
+                                    </ul>
+                                </div>
+                            </article>
+             
+
+           
+            
+                <?php
+                        }
+                    
+                ?>
+       
+    
+               
+            </div>
+
+
+      
+
+   
+
+ 
+
+
+
+
+</body>
+
+</html>
+<?php require 'Footer.php'; ?>
---
+++ Header.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>EvenTN</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="Free Website Template" name="keywords">
+    <meta content="Free Website Template" name="description">
+
+    <!-- Favicon -->
+    <link href="img/etn.png" rel="icon">
+
+    <!-- Font Awesome -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+
+    <!-- Flaticon Font -->
+    <link href="lib/flaticon/font/flaticon.css" rel="stylesheet">
+
+    <!-- Libraries Stylesheet -->
+    <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="css/style7.css" rel="stylesheet">
+</head>
+
+<body class="bg-white">
+    <!-- Topbar Start -->
+    <div class="container-fluid">
+        <div class="row bg-secondary py-2 px-lg-5">
+            <div class="col-lg-6 text-center text-lg-left">
+                <div class="d-inline-flex align-items-center">
+                 
+                </div>
+            </div>
+            <div class="col-lg-6 text-center text-lg-right">
+                <div class="d-inline-flex align-items-center">
+                    <p class="m-0 mr-1 text-white">Follow Us:</p>
+                    <a class="text-white px-2" href="">
+                        <i class="fab fa-facebook-f"></i>
+                    </a>
+                    <a class="text-white px-2" href="">
+                        <i class="fab fa-twitter"></i>
+                    </a>
+                    <a class="text-white px-2" href="">
+                        <i class="fab fa-linkedin-in"></i>
+                    </a>
+                    <a class="text-white px-2" href="">
+                        <i class="fab fa-instagram"></i>
+                    </a>
+                    <a class="text-white px-2" href="">
+                        <i class="fab fa-youtube"></i>
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="row py-3 px-lg-5">
+            <div class="col-lg-4">
+                <a href="" class="navbar-brand d-none d-lg-block">
+                    <!--<h1 class="m-0 display-5 text-capitalize font-italic"><span class="text-primary">Even</span>TN</h1>-->
+                    <img class="w-20" src="img/etn.png" alt="Image">
+                </a>
+            </div>
+            <div class="col-lg-8 text-center text-lg-right">
+                <div class="d-inline-flex align-items-center">
+                    <div class="d-inline-flex flex-column text-center pr-3 border-right">
+                        <p></p>
+                       
+                        <h6>Our Office</h6>
+                        <p class="m-0">123 Street, ariana, tunisia</p>
+                    </div>
+                    <div class="d-inline-flex flex-column text-center px-3 border-right">
+                        <p></p>
+                        
+                        <h6>Email Us</h6>
+                        <p class="m-0">info@EvenTN.com</p>
+                    </div>
+                    <div class="d-inline-flex flex-column text-center pl-3">
+                        <p></p>
+                        <h6>Call Us</h6>
+                        <p class="m-0">+216 20 102 678</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Topbar End -->
+
+
+    <!-- Navbar Start -->
+    <div class="container-fluid p-0 mb-5">
+        <nav class="navbar navbar-expand-lg bg-secondary navbar-dark py-0">
+            <a href="" class="navbar-brand d-block d-lg-none">
+                <h1 class="m-0 display-5 text-capitalize font-italic text-white"><span class="text-primary">Even</span>TN</h1>
+            </a>
+            <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-between" id="navbarCollapse">
+                <div class="navbar-nav m-auto py-4">
+                    <a href="index.html" class="nav-item nav-link">******</a>
+                    <a href="about.html" class="nav-item nav-link">******</a>
+                    <a href="service.html" class="nav-item nav-link">*****</a>
+                    <a href="guard.html" class="nav-item nav-link">******</a>
+                    <div class="nav-item dropdown">
+                        <a href="#" class="nav-item nav-link" >furom</a>
+                      
+                    </div>
+                    <a href="contact.html" class="nav-item nav-link">******</a>
+                </div>
+            </div>
+        </nav>
+    </div>
+    <a href="#" class="btn btn-secondary border back-to-top"><i class="fa fa-angle-double-up"></i></a>
+
+
+<!-- JavaScript Libraries -->
+<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
+<script src="lib/easing/easing.min.js"></script>
+<script src="lib/waypoints/waypoints.min.js"></script>
+<script src="lib/counterup/counterup.min.js"></script>
+<script src="lib/owlcarousel/owl.carousel.min.js"></script>
+
+<!-- Contact Javascript File -->
+<script src="mail/jqBootstrapValidation.min.js"></script>
+<script src="mail/contact.js"></script>
+
+<
+   <script src="js/main.js"></script>
+   </body>
+
+</html>
